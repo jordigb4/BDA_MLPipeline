88403e1934860133d75d576f9338a0217e083c37
Marta-Carrion-Bastida <martacarbas@gmail.com>
2025-03-20 23:11:35 +0100

    pagination

--- data_loaders/electricity_DL.py
+++ data_loaders/electricity_DL.py
@@ -6,34 +6,58 @@ from dotenv import load_dotenv
 load_dotenv()  
 
 def load_data_electricity(start_date: str, end_date: str, output_dir: str = './data'):
-    api_key = os.getenv('API_KEY_ELECTRICITY')  
-    
+    api_key = os.getenv('API_KEY_ELECTRICITY')
     base_url = "https://api.eia.gov/v2/electricity/rto/daily-region-data/data/"
-    params = {
-        "api_key": api_key,
-        "frequency": "daily",
-        "data[0]": "value",
-        "facets[respondent][]": "LDWP",  
-        "start": start_date,
-        "end": end_date,
-        "sort[0][column]": "period",
-        "sort[0][direction]": "desc",
-        "offset": 0,
-        "length": 5000
-    }
-    
+
+    output_file = os.path.join(output_dir, f"electricity_data_{start_date}_{end_date}.json")
+    all_data = []  
+
+    offset = 0
+    length = 5000  # Mida màxima perquè funcioni la api
+
+    os.makedirs(output_dir, exist_ok=True)  
+
+    while True:
+        params = {
+            "api_key": api_key,
+            "frequency": "daily",
+            "data[0]": "value",
+            "facets[respondent][]": "LDWP",
+            "start": start_date,
+            "end": end_date,
+            "sort[0][column]": "period",
+            "sort[0][direction]": "desc",
+            "offset": offset,
+            "length": length
+        }
+
+        response = requests.get(base_url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            filtered_data = [
+                entry for entry in data.get("response", {}).get("data", [])
+                if entry.get("type-name") == "Demand" and entry.get("timezone") == "Pacific"
+            ]
+            all_data.extend(filtered_data)
+
+            print(f"Descargadas {len(filtered_data)} filas (offset {offset})")
+
+            total_rows = int(data["response"].get("total", 0))
+            offset += length  
+
+            if offset >= total_rows:
+                break  
+        else:
+            print(f"Error en la solicitud: {response.status_code}, {response.text}")
+            break
+
   
-    
-    response = requests.get(base_url, params=params)
+    with open(output_file, "w") as file:
+        json.dump(all_data, file, indent=4)
 
-    if response.status_code == 200:
-        os.makedirs(output_dir, exist_ok=True)
-        file_path = os.path.join(output_dir, f"electricity_data_{start_date}_{end_date}.json")
-        with open(file_path, "w") as file:
-            json.dump(response.json(), file, indent=4)
-        print(f"Datos guardados en {file_path}")
-    else:
-        print(f"Error al obtener datos: {response.status_code}, {response.text}")
+    print(f"Datos guardados en {output_file}")
 
 
-load_data_electricity("2019-01-01T00", "2024-03-31T00")
+load_data_electricity("2019-01-01", "2024-03-31")
