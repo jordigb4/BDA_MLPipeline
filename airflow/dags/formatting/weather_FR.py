7ff7ea9c9890033a9adacc1463c348760d3b0e54
jordigb4 <jordi.granja.bayot@gmail.com>
2025-04-10 23:32:20 +0200

    weather

--- airflow/dags/formatting/weather_FR.py
+++ airflow/dags/formatting/weather_FR.py
@@ -1,10 +1,12 @@
 import logging
 import os
+import re
 
 from dags.landing.class_types import WeatherStationId
 from dags.utils.postgres_utils import PostgresManager
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType, LongType
 
 
 def format_weather(postgres_manager: PostgresManager):
@@ -15,20 +17,10 @@ def format_weather(postgres_manager: PostgresManager):
     # Base hdfs path
     base_hdfs_path = f"{os.getenv('HDFS_FS_URL')}/data/landing/weather/"
 
-    # Create hdfs landing path
-    landing_long_beach = base_hdfs_path + WeatherStationId.LONG_BEACH.name
-    landing_downtown = base_hdfs_path + WeatherStationId.DOWNTOWN.name
-    landing_reseda = base_hdfs_path + WeatherStationId.RESEDA.name
-
-    # Create POSTGRESQL table names
-    long_beach_table = f"formatted_{WeatherStationId.LONG_BEACH.name}"
-    downtown_table = f"formatted_{WeatherStationId.DOWNTOWN.name}"
-    reseda_table = f"formatted_{WeatherStationId.RESEDA.name}"
-
-    # Call functions
-    format_station_weather(landing_long_beach, long_beach_table, postgres_manager)
-    format_station_weather(landing_downtown, downtown_table, postgres_manager)
-    format_station_weather(landing_reseda, reseda_table, postgres_manager)
+    for station in [WeatherStationId.LONG_BEACH, WeatherStationId.DOWNTOWN, WeatherStationId.RESEDA]:
+        landing_path = base_hdfs_path + station.name
+        table_name = f"formatted_weather_{station.name}"
+        format_station_weather(landing_path, table_name, postgres_manager)
 
 
 def format_station_weather(landing_path: str, table_name: str, postgres_manager: PostgresManager):
@@ -38,28 +30,59 @@ def format_station_weather(landing_path: str, table_name: str, postgres_manager:
         .appName("ElementDataFormatter") \
         .getOrCreate()
 
-    # Read Parquet files
-    df = spark.read.parquet(landing_path)
+    # 1. Create dataframe from RDD (in hdfs) and schema
+    # =======================================
+    schema = StructType([
+        StructField("STATION", StringType(), True),
+        StructField("DATE", StringType(), True),
+        StructField("OBS_TIME", StringType(), True),
+        StructField("ELEMENT", StringType(), True),
+        StructField("DATA_VALUE", LongType(), True),
+        StructField("parsed_date", DateType(), True)
+    ])
+
+    df = spark.read.schema(schema).parquet(landing_path)
 
     logging.info("Initial Schema:")
     df.printSchema()
     logging.info("\nInitial Sample Data:")
     df.show(5, truncate=False)
 
-    # Convert DATE to standard date format (assuming 'yyyyMMdd' string)
-    df = df.withColumn("DATE", F.to_date(F.col("DATE"), "yyyyMMdd"))
+    # 2. Variable Formatting
+    # ======================
 
-    # Pivot to create columns for each element
-    pivoted_df = df.groupBy("DATE").pivot("ELEMENT").agg(F.first("DATA_VALUE"))
+    # Variable names already correct
 
-    pivoted_df = pivoted_df.orderBy("DATE", ascending=True)
+    # 3. Value Formatting
+    # ===================
+
+    # Date formats: standarize to ISO 8601
+    # Time Zone Alignement: UTC -> already in the data!
+    df = df.withColumn("datetime_iso", F.to_date(F.col("DATE"), "yyyyMMdd"))
+    # Homogenize null indicators
+    poss_nulls = ["NA", "N/A", "null", "", " "]
+    df = df.na.replace(poss_nulls, None)
+
+    # Check non-null constraints
+    df = df.filter(
+        F.col("datetime_iso").isNotNull() &
+        F.col("DATA_VALUE").isNotNull()
+    )
+
+    # 4. Format DataFrame Structure
+    # =============================
+
+    pivoted_df = df.groupBy("datetime_iso").pivot("ELEMENT").agg(F.first("DATA_VALUE"))
+
+    pivoted_df = pivoted_df.orderBy("datetime_iso", ascending=True)
 
     logging.info("Final Schema:")
     pivoted_df.printSchema()
     logging.info("\nSample Data:")
     pivoted_df.show(5, truncate=False)
 
-    # Write to PostgreSQL (update connection details as needed)
+    # 5. Write to PostgreSQL
+    # =============================
     postgres_manager.write_dataframe(pivoted_df, table_name)
 
     spark.stop()
